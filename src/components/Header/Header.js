fcd6d8fac404ae20615faef7e97cae15d4ce2bee
Roy Quilor <royquilor@gmail.com>
2018-02-01 15:46:39 +0000

    Update

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,10 +4,10 @@ import './Header.css'
 const Header = (props) => {
   return (
     <header className="w-100 cf pa3">
-      <h2 className="f2 fw6 fl dib">
+      <h2 className="f3 f2-ns fw6 fl-ns db dib-ns">
         <span className={props.type}>{props.pokemon}</span>
       </h2>
-      <div className="dib fr f3 tr">
+      <div className="db dib-ns fr-ns f3 tr-ns">
         <span className="db"><small>perfect cp</small> {props.regularcp}</span>
         <span className="db"><small>weather boost</small> {props.weatherboostcp}</span>
       </div>
