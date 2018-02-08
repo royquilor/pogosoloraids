48ad77ea8cee38287a06557e107acb37fb5be329
Roy Quilor <royquilor@gmail.com>
2018-02-08 13:22:21 +0000

    Update

--- src/components/SiteNav/SiteNav.js
+++ src/components/SiteNav/SiteNav.js
@@ -7,21 +7,20 @@ import {
 
 const SiteNav = (props) => {
   return (
-    <nav className="nav dt-ns w-100 border-box pv3 pv0-ns">
-      <NavLink to="/" exact={true} className="dtc-ns v-mid mid-gray link dim w-100 w-25-ns logo mb3 mb0-ns" activeClassName="active">SoloRaid</NavLink>
-
-      <div className="nowrap overflow-x-auto overflow-y-hidden dtc-ns v-mid w-100 w-75-ns tr f7 f6-ns">
-        <NavLink to="/Alakazam" className="mr4 mr3-ns" activeClassName="active psychic">Alakazam</NavLink>
-        <NavLink to="/Gengar" className="mr4 mr3-ns" activeClassName="active ghost">Gengar</NavLink>
-        <NavLink to="/Machamp" className="mr4 mr3-ns" activeClassName="active fighting">Machamp</NavLink>
-        <NavLink to="/Ninetales" className="mr4 mr3-ns" activeClassName="active fire">Ninetales</NavLink>
-        <NavLink to="/Omastar" className="mr4 mr3-ns" activeClassName="active water">Omastar</NavLink>
-        <a className="dib submit pv1 ph2 pv2-ns ph3-ns ml3 absolute top-2 right-1 top-0-ns right-0-ns relative-ns" href="">Contact</a>
+    <nav className="nav flex xjustify-center items-center justify-between">
+      <NavLink to="/" exact={true} className="mid-gray link dim logo " activeClassName="active">SoloRaid</NavLink>
+      <div className="f7 f6-ns tc tc-ns">
+        <NavLink to="/Alakazam" className="mh3 pv2" activeClassName="active psychic">Alakazam</NavLink>
+        <NavLink to="/Gengar" className="mh3 pv2" activeClassName="active ghost">Gengar</NavLink>
+        <NavLink to="/Machamp" className="mh3 pv2" activeClassName="active fighting">Machamp</NavLink>
+        <NavLink to="/Ninetales" className="mh3 pv2" activeClassName="active fire">Ninetales</NavLink>
+        <NavLink to="/Omastar" className="mh3 pv2" activeClassName="active water">Omastar</NavLink>
         {/*
         <NavLink to="/Porygon" activeClassName="active ">Porygon</NavLink>
         <NavLink to="/Scyther" activeClassName="active ">Scyther</NavLink>
         */}
       </div>
+      <a className="dib submit pv1 ph2 pv2-ns ph3-ns " href="">Contact</a>
 
     </nav>
   )
