19871b8801fe3d9b73df0b2a78ceb4b019f2e44f
Roy Quilor <royquilor@gmail.com>
2018-02-09 12:45:34 +0000

    Update

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -16,13 +16,13 @@ class Home extends Component {
     return (
       <BodyClassName className="home">
       <div className="black-70">
-        <div className="w-100 cf tc-m hero pv4 pa3-ns pv6-ns">
+        <div className="w-100 cf tc-m hero pv4 pa3 pv6-ns">
           <div className="mw7 center tl ">
             <div className="tl mb4-ns w-50-ns fl-ns">
-              <h1 className="f4 fw6 f1-ns lh-solid mb3 mt0 xttu">Level 3 Solo Hall of Fame</h1>
+              <h1 className="f2 fw6 f1-ns lh-solid mb3 mt0 ttu">Level 3 Solo Hall of Fame</h1>
             </div>
             <div className="tl mb4-ns w-50-ns fl-ns">
-              <p className="f6 f5-ns lh-copy measure center db">Pokemon go stuff gathered by a community. 6/7 raid bosses solo'd with level 30 pokemon without weather boost.</p>
+              <p className="f6 f5-ns lh-copy measure center db ">Pokemon go stuff gathered by a community. 6/7 raid bosses solo'd with level 30 pokemon without weather boost.</p>
 
             </div>
           </div>
--- src/components/SiteNav/SiteNav.js
+++ src/components/SiteNav/SiteNav.js
@@ -7,14 +7,14 @@ import {
 
 const SiteNav = (props) => {
   return (
-    <nav className="nav flex xjustify-center items-center justify-between">
-      <NavLink to="/" exact={true} className="link dim logo " activeClassName="active">SoloRaid</NavLink>
-      <div className="f7 f6-ns tc tc-ns">
-        <NavLink to="/Alakazam" className="link dim mh2 pv2 " activeClassName="active psychic">Alakazam</NavLink>
-        <NavLink to="/Gengar" className="link dim mh2 pv2" activeClassName="active ghost">Gengar</NavLink>
-        <NavLink to="/Machamp" className="link dim mh2 pv2" activeClassName="active fighting">Machamp</NavLink>
-        <NavLink to="/Ninetales" className="link dim mh2 pv2" activeClassName="active fire">Ninetales</NavLink>
-        <NavLink to="/Omastar" className="link dim mh2 pv2" activeClassName="active water">Omastar</NavLink>
+    <nav className="pa3 nav flex xjustify-center items-center justify-between flex-column flex-row-l">
+      <NavLink to="/" exact={true} className="link dim logo f5 f4-ns lh-solid" activeClassName="active">SoloRaid</NavLink>
+      <div className="f7 f6-ns tc tc-ns pv3 pv0-l">
+        <NavLink to="/Alakazam" className="link dim mh2 pv3 pv0-ns" activeClassName="active psychic">Alakazam</NavLink>
+        <NavLink to="/Gengar" className="link dim mh2 pv3 pv0-ns" activeClassName="active ghost">Gengar</NavLink>
+        <NavLink to="/Machamp" className="link dim mh2 pv3 pv0-ns" activeClassName="active fighting">Machamp</NavLink>
+        <NavLink to="/Ninetales" className="link dim mh2 pv3 pv0-ns" activeClassName="active fire">Ninetales</NavLink>
+        <NavLink to="/Omastar" className="link dim mh2 pv3 pv0-ns" activeClassName="active water">Omastar</NavLink>
         {/*
         <NavLink to="/Porygon" activeClassName="active ">Porygon</NavLink>
         <NavLink to="/Scyther" activeClassName="active ">Scyther</NavLink>
