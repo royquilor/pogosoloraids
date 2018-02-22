63c3c53f68bc0c6cd68ccb351f2298e8f808161f
Roy Quilor <royquilor@gmail.com>
2018-02-22 00:12:03 +0000

    Update

--- src/components/SiteNav/SiteNav.js
+++ src/components/SiteNav/SiteNav.js
@@ -10,10 +10,10 @@ const SiteNav = (props) => {
     <nav className="pa3 nav flex xjustify-center items-center justify-between flex-column flex-row-l relative">
       <NavLink to="/" exact={true} className="link dim logo f5 f4-ns pt1 pto-ns lh-solid absolute top-0 left-0 relative-l" activeClassName="active">SoloRaid</NavLink>
       <div className="f7 f6-ns tc tc-ns pv3 pv0-l">
-        <NavLink to="/Alakazam" className="link dim mh2 pv3 pv0-ns" activeClassName="active psychic">Alakazam</NavLink>
-        <NavLink to="/Gengar" className="link dim mh2 pv3 pv0-ns" activeClassName="active ghost">Gengar</NavLink>
         <NavLink to="/Machamp" className="link dim mh2 pv3 pv0-ns" activeClassName="active fighting">Machamp</NavLink>
-        <NavLink to="/Ninetales" className="link dim mh2 pv3 pv0-ns" activeClassName="active fire">Ninetales</NavLink>
+        <NavLink to="/Piloswine" className="link dim mh2 pv3 pv0-ns" activeClassName="active ice">Piloswine</NavLink>
+        <NavLink to="/Jynx" className="link dim mh2 pv3 pv0-ns" activeClassName="active ice">Jynx</NavLink>
+        <NavLink to="/Azumarill" className="link dim mh2 pv3 pv0-ns" activeClassName="active water">Azumarill</NavLink>
         {/*
         <NavLink to="/Porygon" activeClassName="active ">Porygon</NavLink>
         <NavLink to="/Scyther" activeClassName="active ">Scyther</NavLink>
