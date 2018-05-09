6fb3a84535e4395a56b26563a0a85f354ad34076
Roy Quilor <royquilor@gmail.com>
2018-05-09 13:05:27 +0100

    Update

--- src/components/Alakazam/Alakazam.js
+++ src/components/Alakazam/Alakazam.js
@@ -1,27 +1,13 @@
 import React, { Component } from 'react';
 import BodyClassName from 'react-body-classname';
 
-import alakazamcfb from '../../data/alakazam/alakazamConfusionFocusBlast';
-import alakazamcfs from '../../data/alakazam/alakazamConfusionFutureSight';
-import alakazamcsb from '../../data/alakazam/alakazamConfusionShadowBall';
-import alakazampcfb from '../../data/alakazam/alakazamPsychoCutFocusBlast';
-import alakazampcfs from '../../data/alakazam/alakazamPsychoCutFutureSight';
-import alakazampcsb from '../../data/alakazam/alakazamPsychoCutShadowBall';
-
-import AlakazamConfusionFocusBlastList from './AlakazamConfusionFocusBlastList';
-import AlakazamConfusionFutureSightList from './AlakazamConfusionFutureSightList';
-import AlakazamConfusionShadowBallList from './AlakazamConfusionShadowBallList';
-import AlakazamPsychoCutFocusBlastList from './AlakazamPsychoCutFocusBlastList';
-import AlakazamPsychoCutFutureSightList from './AlakazamPsychoCutFutureSightList';
-import AlakazamPsychoCutShadowBallList from './AlakazamPsychoCutShadowBallList';
+import AlakazamExtremeTable from './AlakazamExtremeTable';
+import AlakazamWindyTable from './AlakazamWindyTable';
 
 import Wrap from '../Wrap/Wrap';
 import Header from '../Header/Header';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from 'react-router-dom';
+
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 
 class Alakazam extends Component {
 
@@ -38,23 +24,19 @@ class Alakazam extends Component {
             likes="Windy"
             />
           <Wrap>
-          <nav class="pb4 pt2 tc">
-          <Router>
-            <Switch>
-              <Router path="/" component
-            </Switch>
-          </Router>
-  <div class="tabs mb4 ttu dib">
-    <a class="link dib mr3 pv3 ph4 ma0" href="#" title="Link 1">Extreme</a>
-    <a class="link dib mr3 pv3 ph4 ma0" href="#" title="Link 1">Windy</a>
-  </div>
-</nav>
-          <AlakazamConfusionFocusBlastList alakazamcfb={alakazamcfb} />
-          <AlakazamConfusionFutureSightList alakazamcfs={alakazamcfs} />
-          <AlakazamConfusionShadowBallList alakazamcsb={alakazamcsb} />
-          <AlakazamPsychoCutFocusBlastList alakazampcfb={alakazampcfb} />
-          <AlakazamPsychoCutFutureSightList alakazampcfs={alakazampcfs} />
-          <AlakazamPsychoCutShadowBallList alakazampcsb={alakazampcsb} />
+            <Tabs>
+              <TabList>
+                <Tab>Extreme</Tab>
+                <Tab>Windy</Tab>
+              </TabList>
+
+              <TabPanel>
+                <AlakazamExtremeTable />
+              </TabPanel>
+              <TabPanel>
+                <AlakazamWindyTable />
+              </TabPanel>
+            </Tabs>
           </Wrap>
         </div>
       </BodyClassName>
---
+++ src/components/Alakazam/AlakazamWindyTable.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+import alakazamcfb from '../../data/alakazam/alakazamConfusionFocusBlast';
+import alakazamcfs from '../../data/alakazam/alakazamConfusionFutureSight';
+import alakazamcsb from '../../data/alakazam/alakazamConfusionShadowBall';
+import alakazampcfb from '../../data/alakazam/alakazamPsychoCutFocusBlast';
+import alakazampcfs from '../../data/alakazam/alakazamPsychoCutFutureSight';
+import alakazampcsb from '../../data/alakazam/alakazamPsychoCutShadowBall';
+
+import AlakazamConfusionFocusBlastList from './AlakazamConfusionFocusBlastList';
+import AlakazamConfusionFutureSightList from './AlakazamConfusionFutureSightList';
+import AlakazamConfusionShadowBallList from './AlakazamConfusionShadowBallList';
+import AlakazamPsychoCutFocusBlastList from './AlakazamPsychoCutFocusBlastList';
+import AlakazamPsychoCutFutureSightList from './AlakazamPsychoCutFutureSightList';
+import AlakazamPsychoCutShadowBallList from './AlakazamPsychoCutShadowBallList';
+
+class AlakazamWindyTable extends Component {
+
+  render() {
+
+    return (
+        <div>
+          <AlakazamConfusionFocusBlastList alakazamcfb={alakazamcfb} />
+          <AlakazamConfusionFutureSightList alakazamcfs={alakazamcfs} />
+          <AlakazamConfusionShadowBallList alakazamcsb={alakazamcsb} />
+          <AlakazamPsychoCutFocusBlastList alakazampcfb={alakazampcfb} />
+          <AlakazamPsychoCutFutureSightList alakazampcfs={alakazampcfs} />
+          <AlakazamPsychoCutShadowBallList alakazampcsb={alakazampcsb} />
+        </div>
+    );
+
+  }
+
+}
+
+export default AlakazamWindyTable;
